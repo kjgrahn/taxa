ed9b1860be209cf407de176b33b1c04b7cee03fd
Jörgen Grahn <grahn+src@snipabacken.se>
2025-02-15 08:25:15 +0100

    taxa: clean up names before printing them
    - s/subsp./ssp./
    - s/x/×/
    
    This makes us print a bit fewer useless names (20% in Orchidaceae)
    because some entries just differ in these ways.

--- names.cc
+++ names.cc
@@ -2,31 +2,73 @@
 
 #include <iostream>
 #include <algorithm>
+#include <cstring>
+#include <unordered_set>
 
 #include "indent.h"
 
 using dyntaxa::Names;
+
+namespace {
+
+    /* Replace all strings 'a' with 'b' in 's', except those created
+     * by the changes themselves.
+     */
+    void replace(std::string& s, const char* a, const char* b)
+    {
+	size_t p = 0;
+
+	while (1) {
+	    auto i = s.find(a, p);
+	    if (i==s.npos) return;
+
+	    s.replace(i, std::strlen(a), b);
+	    p = i + std::strlen(b);
+	}
+    }
+
+    std::string normalize(std::string name)
+    {
+	replace(name, " subsp. ", " ssp. ");
+	replace(name, " x ", u8" \u00d7 ");
+	return name;
+    }
+}
 
 std::ostream& Names::put(std::ostream& os,
 			 Indent& indent) const
 {
     auto is_preferred = [] (auto name) { return name->preferred=="true"; };
 
+    std::unordered_set<std::string> mem;
+    auto seen = [&mem] (auto s) {
+	return !mem.insert(s).second;
+    };
+
     auto pit = std::find_if(begin(names), end(names), is_preferred);
     if (pit==end(names)) pit = begin(names);
 
-    if (pit==end(names)) {
-	return indent.ljust(os, "-", 23) << " (" << taxon.name << ")\n";
-    }
+    const auto name = normalize(taxon.name);
+    seen(name);
 
-    indent.ljust(os, (*pit)->name, 23) << " (" << taxon.name << ")\n";
+    if (pit==end(names)) {
+	indent.ljust(os, "-", 23) << " (" << name << ")\n";
+    }
+    else {
+	indent.ljust(os, (*pit)->name, 23) << " (" << name << ")\n";
+    }
 
     for (auto it=begin(names); it!=end(names); it++) {
 	if (it==pit) continue;
-	os << "= " << (*it)->name << '\n';
+	const auto name = normalize((*it)->name);
+	if (seen(name)) continue;
+	os << "= " << name << '\n';
     }
+
     for (auto& syn : synonyms) {
-	indent.ljust(os, "= ", 25) << syn->name << '\n';
+	const auto name = normalize(syn->name);
+	if (seen(name)) continue;
+	indent.ljust(os, "= ", 25) << name << '\n';
     }
     return os;
 }
